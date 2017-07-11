e0431f3c7e1e23141b28db7e5332be3d5552ea6e
phhui <phhui@qq.com>
2017-07-11 18:02:40 +0800

    up

--- DBUtilsDemo/testMySql.py
+++ DBUtilsDemo/testMySql.py
@@ -1,7 +1,5 @@
 #-*- coding: UTF-8 -*-  
 from DB_connetion_pool import *;
-import json
-
 def testMySQL():  
     #申请资源  
     with getPTConnection() as db:
@@ -10,12 +8,11 @@ def testMySQL():
             # 获取所有记录列表
         db.cursor.execute(sql)
         results = db.cursor.fetchall()
-        print json.dumps(results)
-#        for row in results:
-#            userId = row[0]
-#            name= row[1]     
-#            sex= row[2]
-#            createTime = row[3]
+        for row in results:
+            userId = row[0]
+            name= row[1]     
+            sex= row[2]
+            createTime = row[3]
             # 打印结果
-#            print "userId="+str(userId)+",name="+str(name)+",sex="+str(sex)+"time="+str(createTime)
+            print ("userId="+userId+",name="+name+",sex="+sex+"time="+createTime)
 testMySQL()
